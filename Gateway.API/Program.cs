a93021e7a7f893b08e9cc7c22ed17288fe679c23
EHV DireWolf <jason.spaninks@gmail.com>
2025-06-03 20:35:59 +0200

    Update program.cs

--- Gateway.API/Program.cs
+++ Gateway.API/Program.cs
@@ -1,6 +1,18 @@
+using System.Security.Cryptography.X509Certificates;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Load certificate manually from file
+var certPath = builder.Configuration["ASPNETCORE_Kestrel__Certificates__Default__Path"];
+var certPassword = builder.Configuration["ASPNETCORE_Kestrel__Certificates__Default__Password"];
+
+if (!string.IsNullOrEmpty(certPath) && File.Exists(certPath)) {
+    builder.WebHost.ConfigureKestrel(options => {
+        options.ConfigureHttpsDefaults(httpsOptions => {
+            httpsOptions.ServerCertificate = new X509Certificate2(certPath, certPassword);
+        });
+    });
+}
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -12,11 +24,7 @@ builder.Services.AddReverseProxy()
 var app = builder.Build();
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.MapReverseProxy();
-
 app.Run();

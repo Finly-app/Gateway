e143573d2bafc73ea1325d49b2c6b317faa293c5
EHV DireWolf <jason.spaninks@gmail.com>
2025-06-03 21:18:23 +0200

    Updates

--- Gateway.API/Program.cs
+++ Gateway.API/Program.cs
@@ -1,23 +1,15 @@
-using System.Security.Cryptography.X509Certificates;
-
 var builder = WebApplication.CreateBuilder(args);
 
-var certPath = builder.Configuration["ASPNETCORE_Kestrel__Certificates__Default__Path"];
-var certPassword = builder.Configuration["ASPNETCORE_Kestrel__Certificates__Default__Password"];
-
-if (!string.IsNullOrEmpty(certPath) && File.Exists(certPath)) {
-    builder.WebHost.ConfigureKestrel(options => {
-        options.ConfigureHttpsDefaults(httpsOptions => {
-            httpsOptions.ServerCertificate = new X509Certificate2(certPath, certPassword);
-        });
+// Configure named HttpClient that skips SSL validation
+builder.Services
+    .AddHttpClient("insecure")
+    .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler {
+        ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
     });
-}
 
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddReverseProxy()
+// Add Reverse Proxy with config
+builder.Services
+    .AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 var app = builder.Build();

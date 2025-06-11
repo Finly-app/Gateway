e31b58decb89e1fa7de7a73c067a81253bd19bd7
EHV DireWolf <jason.spaninks@gmail.com>
2025-06-11 18:11:38 +0200

    Updated Program

--- Gateway.API/Program.cs
+++ Gateway.API/Program.cs
@@ -1,17 +1,27 @@
 var builder = WebApplication.CreateBuilder(args);
 
+var allowedOrigins = builder.Configuration.GetSection("Cors:AllowedOrigins").Get<string[]>();
+
+Console.WriteLine("Origins: " + string.Join(", ", allowedOrigins));
+
+builder.Services.AddCors(options => {
+    options.AddPolicy("AllowFrontend", policy => {
+        policy.WithOrigins(allowedOrigins)
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 builder.Services
     .AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
-//builder.WebHost.ConfigureKestrel(options => {
-//    options.ListenAnyIP(8080);
-//});
-
 builder.Services.AddControllers();
+
 var app = builder.Build();
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
+app.UseCors("AllowFrontend");
 app.UseAuthorization();
 
 app.MapReverseProxy();
